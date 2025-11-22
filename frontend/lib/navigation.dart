8c12d4e4a97a358f97f22cb984f1f170fc5a21cb
Aryan Nagdawane <artisticaryan77@gmail.com>
2025-11-22 14:38:38 +0530

    added location

--- frontend/lib/navigation.dart
+++ frontend/lib/navigation.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
-class Navigation extends StatelessWidget {
+import 'package:geolocator/geolocator.dart';
+
+class Navigation extends StatefulWidget {
   const Navigation({super.key});
 
   @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  double? lat;
+  double? lng;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    Position pos = await Geolocator.getCurrentPosition(
+      //desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      lat = pos.latitude;
+      lng = pos.longitude;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Navigation'),
-        ),
+        appBar: AppBar(title: const Text('Navigation')),
         body: Center(
-          child: Text('Navigation content goes here'),
+          child: lat == null
+              ? const CircularProgressIndicator()
+              : Text('Latitude: $lat, Longitude: $lng'),
         ),
       ),
     );
